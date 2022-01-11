70885d541ec0074209d92a23e94d94f6613c6dbe
BitJKit <ionathan.m.kelly@gmail.com>
2022-01-11 18:40:10 -0500

    git final
    Create final version for grading

---
+++ app/src/main/java/com/example/edittext/DisplayMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.example.edittext
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DisplayMessageActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        var intent = intent
+        val fName = intent.extras?.get(FNAME_EXTRA) ?: "0"
+        val lName = intent.extras?.get(LNAME_EXTRA) ?: "1"
+        val email = intent.extras?.get(EMAIL_EXTRA) ?: "2"
+
+        val resultTv = findViewById<TextView>(R.id.resultTv)
+        resultTv.text = "First Name: " + fName+ "\n Last name: "+ lName + "\nEmail: "+ email
+    }
+}
